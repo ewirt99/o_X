66087eab7fbc08c4d4f34d615d057a491cd4d0c5
Eli Wirtshafter <ewirt@wharton.upenn.edu>
2016-06-30 00:34:56 +0200

    hell yaw

---
+++ o_X/OXGameController.swift
@@ -0,0 +1,13 @@
+//
+//  OXGameController.swift
+//  o_X
+//
+//  Created by Eli Wirtshafter on 6/29/16.
+//  Copyright © 2016 iX. All rights reserved.
+//
+
+import UIKit
+
+class OXGameController: NSObject {
+
+}
